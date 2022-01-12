64a9f6183fba65835d7a356f6b4ea998a8d40b3a
Rafael Sanzio <rafaelsanzio27@gmail.com>
2022-01-12 09:53:28 -0300

    test: register user on mailing list
    test: register user on mailing list

---
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -0,0 +1,19 @@
+import { UserData } from './user-data'
+
+describe('Register user on mailing list use case', () => {
+  test('should add user with complete data to mailing list', async () => {
+    const users: UserData[] = []
+    console.log(users)
+
+    /* const repo: UserRepository = new InMemoryUserRepository(users)
+    const useCase: RegisterUserOnMailingList = new RegisterUserOnMailingListUseCase(repo)
+
+    const name = 'any_name'
+    const email = 'any@email.com'
+
+    const response = await useCase.registerUserOnMailingList({ name, email })
+    const user = repo.findUserByEmail(email)
+
+    expect((await user).name).toBe(name) */
+  })
+})
