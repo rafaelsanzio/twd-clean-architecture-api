c3755c98ac09ad195b9f8b11eaa16fedcec62dbd
Rafael Sanzio <rafaelsanzio27@gmail.com>
2022-01-13 09:20:13 -0300

    test: validate local part larger than 64 chars
    test: validate local part larger than 64 chars

--- src/entities/email-validation.spec.ts
+++ src/entities/email-validation.spec.ts
@@ -18,4 +18,10 @@ describe('Email validation', () => {
 
     expect(Email.validate(email)).toBeTruthy()
   })
+
+  test('should not accept local part larger than 64 chars', () => {
+    const email: string = 'l'.repeat(65) + '@mail.com'
+
+    expect(Email.validate(email)).toBeFalsy()
+  })
 })
