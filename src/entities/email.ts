4d3dafa08024ce4ec49a87aab9f7f5dcb29e63d2
Rafael Sanzio <rafaelsanzio27@gmail.com>
2022-01-17 09:36:32 -0300

    feat: create email method
    feat: create email method

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -1,4 +1,21 @@
+import { Either, left, right } from '../shared/either'
+import { InvalidEmailError } from './errors/invalid-email-error'
+
 export class Email {
+  private readonly email: string
+
+  constructor(email: string) {
+    this.email = email
+  }
+
+  static create(email: string): Either<InvalidEmailError, Email> {
+    if (!Email.validate(email)) {
+      return left(new InvalidEmailError())
+    }
+
+    return right(new Email(email))
+  }
+
   static validate(email: string): boolean {
     const emailRegex =
       /^[-!#$%&'*+/0-9=?A-Zˆ_a-z`{|}~](\.?[-!#$%&'*+/0-9=?A-Zˆ_a-z`{|}~])*@[a-zA-Z0-9](-?\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/
