bba67d39681261cc3e02a92540f2ac5856aacefb
Rafael Sanzio <rafaelsanzio27@gmail.com>
2022-01-17 11:23:12 -0300

    test: create a user with name invalid
    test: create a user with name invalid

--- src/entities/user.spec.ts
+++ src/entities/user.spec.ts
@@ -1,5 +1,6 @@
 import { left } from '../shared/either'
 import { InvalidEmailError } from './errors/invalid-email-error'
+import { InvalidNameError } from './errors/invalid-name-error'
 import { User } from './user'
 
 describe('User domain entity', () => {
@@ -8,5 +9,15 @@ describe('User domain entity', () => {
     const error = User.create({ name: 'any_name', email: invalidEmail })
 
     expect(error).toEqual(left(new InvalidEmailError()))
+  })
+
+  test('should not create user with invalid names', async () => {
+    const invalidNames = ['O      ', 'O'.repeat(257)]
+
+    for (const invalidName of invalidNames) {
+      const error = User.create({ name: invalidName, email: 'any@mail.com' })
+
+      expect(error).toEqual(left(new InvalidNameError()))
+    }
   })
 })
