1d4519acb97d763c39ae4070c6196811ae93d443
Rafael Sanzio <rafaelsanzio27@gmail.com>
2022-01-27 08:29:21 -0300

    test: apply content type middleware
    test: apply content type middleware

---
+++ test/main/middlewares/content-type.test.ts
@@ -0,0 +1,21 @@
+import request from 'supertest'
+import app from '@/main/config/app'
+
+describe('Content type Middleware', () => {
+  test('should return default content type as JSON', async () => {
+    app.get('/test-content-type', (req, res) => {
+      res.send('')
+    })
+
+    await request(app).get('/test-content-type').expect('content-type', /json/)
+  })
+
+  test('should return XML content type when forced', async () => {
+    app.get('/test-content-type-xml', (req, res) => {
+      res.type('xml')
+      res.send('')
+    })
+
+    await request(app).get('/test-content-type-xml').expect('content-type', /xml/)
+  })
+})
