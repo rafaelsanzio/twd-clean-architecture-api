b11fec2e4195d2ccb7747b27d34253ebbe75d6a3
Rafael Sanzio <rafaelsanzio27@gmail.com>
2022-01-27 08:23:26 -0300

    refactor: test route
    refactor: test route

--- test/main/middlewares/body-parser.test.ts
+++ test/main/middlewares/body-parser.test.ts
@@ -3,10 +3,10 @@ import app from '@/main/config/app'
 
 describe('Body parser middleware', () => {
   test('should parse body as json', async () => {
-    app.post('/test_body_parser', (req, res) => {
+    app.post('/test-body-parser', (req, res) => {
       res.send(req.body)
     })
 
-    await request(app).post('/test_body_parser').send({ name: 'Rafael' }).expect({ name: 'Rafael' })
+    await request(app).post('/test-body-parser').send({ name: 'Rafael' }).expect({ name: 'Rafael' })
   })
 })
