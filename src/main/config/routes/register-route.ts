0b393cb15d7e42378d9b28ce150f8c5c0ed2ee74
Rafael Sanzio <rafaelsanzio27@gmail.com>
2022-01-27 09:05:14 -0300

    feat: config register routes
    feat: config register routes

---
+++ src/main/config/routes/register-route.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express'
+
+import { makeRegisterUserController } from '@/main/factories'
+import { adaptRoute } from '@/main/config/adapters'
+
+export default (router: Router): void => {
+  router.post('/register', adaptRoute(makeRegisterUserController()))
+}
