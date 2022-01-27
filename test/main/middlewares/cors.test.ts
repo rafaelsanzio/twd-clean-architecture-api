cf806c4038ec2751726715f03af7047fd6fab1e3
Rafael Sanzio <rafaelsanzio27@gmail.com>
2022-01-27 08:23:14 -0300

    test: apply cors enabling middleware
    test: apply cors enabling middleware

---
+++ test/main/middlewares/cors.test.ts
@@ -0,0 +1,17 @@
+import request from 'supertest'
+
+import app from '@/main/config/app'
+
+describe('cors middleware', () => {
+  test('should enable CORS', async () => {
+    app.post('/test-cors', (req, res) => {
+      res.send()
+    })
+
+    await request(app)
+      .get('/test-cors')
+      .expect('access-control-allow-origin', '*')
+      .expect('access-control-allow-headers', '*')
+      .expect('access-control-allow-methods', '*')
+  })
+})
