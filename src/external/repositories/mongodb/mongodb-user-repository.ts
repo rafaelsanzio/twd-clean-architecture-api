4f47e6c8eb602b560c14ccd70707c238d74efabd
Rafael Sanzio <rafaelsanzio27@gmail.com>
2022-01-31 09:37:51 -0300

    feat: add user and verify with mongodb
    feat: add user and verify with mongodb

---
+++ src/external/repositories/mongodb/mongodb-user-repository.ts
@@ -0,0 +1,31 @@
+import { UserData } from '@/entities'
+import { UserRepository } from '@/usecases/register-user-on-mailing-list/ports'
+import { MongoHelper } from './helper'
+
+export class MongodbUserRepository implements UserRepository {
+  async add(user: UserData): Promise<void> {
+    const userCollection = MongoHelper.getCollection('users')
+    const userExist = await this.exists(user.email)
+    if (!userExist) {
+      await userCollection.insertOne(user)
+    }
+  }
+
+  findUserByEmail(email: string): Promise<UserData> {
+    const userCollection = MongoHelper.getCollection('users')
+    const user = userCollection.findOne({ email })
+    return user || null
+  }
+
+  findAllUsers(): Promise<UserData[]> {
+    throw new Error('Method not implemented.')
+  }
+
+  async exists(email: string): Promise<boolean> {
+    const foundUser = await this.findUserByEmail(email)
+    if (foundUser === null) {
+      return false
+    }
+    return true
+  }
+}
